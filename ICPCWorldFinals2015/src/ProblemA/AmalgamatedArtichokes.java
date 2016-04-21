82d1e5fb264eb717614edaba31907cd5b6ec460c
lukasleung <lleung@clarku.edu>
2016-04-21 16:41:33 -0400

    update

--- ICPCWorldFinals2015/src/ProblemA/AmalgamatedArtichokes.java
+++ ICPCWorldFinals2015/src/ProblemA/AmalgamatedArtichokes.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /**
  * UVA 1709 ICPC Problem A 2015 World Finals Timelimit = 5.000 seconds
  * Completed testing in: 0. seconds on
@@ -7,33 +6,24 @@ import java.util.Scanner;
  *      javac AmalgamatedArtichokes.java
  *      java AmalgamatedArtichokes < pathTo/input.txt > printToThis.txt
  * @author Lukas Leung
- * @version 2.0
+ * @version 3.0
  */
 public class AmalgamatedArtichokes {
     private int p, a, b, c, d;
+    private double pi = 2*Math.PI;
     public AmalgamatedArtichokes() {
         Scanner in = new Scanner(System.in);
-        int round = 0;
-        StringBuilder out = new StringBuilder();
         while (in.hasNextInt()) {
-            if (round++ != 0) {
-//                System.out.println();
-                out.append("\n");
-            }
-//            long i = System.nanoTime();
             p = in.nextInt();
             a = in.nextInt();
             b = in.nextInt();
             c = in.nextInt();
             d = in.nextInt();
             int n = in.nextInt();
-//            long read = (System.nanoTime() - i);
-//            System.out.println("read: " + read);
             double val = f(1),
                     h = val,
                     l = val,
                     diff = 0;
-//            i = System.nanoTime();
             for (int x = 2; x < n; x++) {
                 val = f(x);
                 if (val > h) { // higher than highest, reset
@@ -42,20 +32,16 @@ public class AmalgamatedArtichokes {
                 } else if (val < l) { // lower than current lowest, check diff
                     l = val;
                     double curdiff = h - l;
-                    if (curdiff > diff) {
-                        diff = curdiff;
-                    }
+                    if (curdiff > diff) { diff = curdiff; }
                 }
             }
-//            long comp = (System.nanoTime() - i);
-//            System.out.println("comp: " + comp);
-//            System.out.printf("%.6f", p*(diff));
-            out.append((p*diff));
+            System.out.printf("%.6f\n", p*(diff));
         }
-        System.out.print(out.toString());
     }
     private double f(int x) {
-        return (Math.sin(a*x+b)+Math.cos(c*x+d));
+        double ab = (a*x+b) % pi,
+               cd = (c*x+d) % pi;
+        return (Math.sin(ab)+Math.cos(cd));
     }
     public static void main(String[] args) {
         AmalgamatedArtichokes solve = new AmalgamatedArtichokes();
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# cs255
+# cs255 Advanced Algorithms CLARK UNIVERSITY Spring 2016
+
+###Goal:  
+#####To futher understanding of complex algorithms in how to formulate them, proove their correctness, and analyze their time and space efficiencies. 
+___
+#### Course Material: [Reference Slides](http://www.cs.clarku.edu/~lhan/courses/cs255/references.php)
++ Greedy
++ String Sorting
++ Tries
++ Data Compression
++ Complete Search
++ Divide and Conquer
++ Dynamic Programming
++ Network Flow
++ P, NP, and NP-Complete
+___
+#### Assignments:
+##### 1. [Turtle Graphics and Anagrams](http://www.cs.clarku.edu/~lhan/courses/cs255/hw1.pdf)
+
+##### 2. [Burrows-Wheeler Compression](http://www.cs.princeton.edu/courses/archive/fall15/cos226/assignments/burrows.html)
+
+##### 3. 
+
+##### 4.
+##### 5.
+##### 6.
+##### 7.
+##### 8.
