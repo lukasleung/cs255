84d291671cf80849fd87e9d6f8fabd619ef75485
lukasleung <lleung@clarku.edu>
2016-05-03 21:13:12 -0400

    update

--- ICPCWorldFinals2015/src/ProblemF/Keyboarding.java
+++ ICPCWorldFinals2015/src/ProblemF/Keyboarding.java
@@ -24,55 +24,134 @@ public class Keyboarding {
         while (in.hasNextLine()) {
             String line = in.nextLine();
             init(line);
-            int index = 0;
+//            int index = 0;
             for (int r = 0; r < R; r++) {
                 line = in.nextLine();
                 for (int c = 0; c < C; c++) {
                     char letter = line.charAt(c);
-                    if (indices.containsKey(letter)) {
-                        String s = indices.get(letter),
-                              ns = s.concat(" " + index);
-                        indices.put(letter, ns);
-//                        System.out.println(indices.get(letter));
-                        int newNum = num.get(letter) + 1;
-                        num.put(letter, newNum);
-//                        System.out.println(num.get(letter));
-                    } else {
-                        indices.put(letter, "" + index);
-                        num.put(letter, 1);
-                    }
+//                    if (indices.containsKey(letter)) {
+//                        String s = indices.get(letter),
+//                              ns = s.concat(" " + index);
+//                        indices.put(letter, ns);
+////                        System.out.println(indices.get(letter));
+//                        int newNum = num.get(letter) + 1;
+//                        num.put(letter, newNum);
+////                        System.out.println(num.get(letter));
+//                    } else {
+//                        indices.put(letter, "" + index);
+//                        num.put(letter, 1);
+//                    }
                     keyboard[r][c] = letter;
-                    index++;
+//                    index++;
                 }
             }
-            // now build adj
-            long start = System.currentTimeMillis();
-            build();
-            long stop = System.currentTimeMillis();
-            System.err.println("Adj: " + (stop - start));
+            line = in.nextLine().concat("*");
 //            printkeyboard();
-//            printadj();
-//            System.out.println("\n\n");
-            //fw();
-//            printadj();
-            line = in.nextLine().concat("*"); // string to find
 //            System.out.println(line);
-            start = System.currentTimeMillis();
-            buildGraph(line);
-            stop = System.currentTimeMillis();
-            System.err.println("G  : " + (stop - start));
-//            System.out.println("\n\n");
-//            printgraph();
-//            System.out.println("\n\n");
-//            printgraph();
-            int minValue = Integer.MAX_VALUE;
-            for (int i = (graph.length-num.get('*')); i < graph.length; i++) {
-                 minValue = Math.min(graph[i], minValue);
-            }
-//            System.out.println(minValue);
-            System.out.println(minValue + line.length());
+            System.out.println(bfs(line) + line.length());
+            // now build adj
+//            long start = System.currentTimeMillis();
+//            build();
+//            long stop = System.currentTimeMillis();
+//            System.err.println("Adj: " + (stop - start));
+////            printkeyboard();
+////            printadj();
+////            System.out.println("\n\n");
+//            //fw();
+////            printadj();
+//            line = in.nextLine().concat("*"); // string to find
+////            System.out.println(line);
+//            start = System.currentTimeMillis();
+//            buildGraph(line);
+//            stop = System.currentTimeMillis();
+//            System.err.println("G  : " + (stop - start));
+////            System.out.println("\n\n");
+////            printgraph();
+////            System.out.println("\n\n");
+////            printgraph();
+//            int minValue = Integer.MAX_VALUE;
+//            for (int i = (graph.length-num.get('*')); i < graph.length; i++) {
+//                 minValue = Math.min(graph[i], minValue);
+//            }
+////            System.out.println(minValue);
+//            System.out.println(minValue + line.length());
         }
     }
+
+
+    private int bfs(String line) {
+        int length = line.length();
+        boolean[][][] seen  = new boolean[length][keyboard.length][keyboard[0].length];
+        Queue<Integer> q = new LinkedList<Integer>();
+        // 0: index in string
+        if (line.charAt(0) == keyboard[0][0]) { q.add(1); }
+        else { q.add(0); }
+        q.add(0); // 1: row
+        q.add(0); // 2: column
+        q.add(0); // 3: length thus far
+        seen[0][0][0] = true;
+        while (!q.isEmpty()) {
+            int index = q.remove(),
+                  row = q.remove(),
+                  col = q.remove(),
+                    l = q.remove();
+//            System.out.println("Searching: " + line.charAt(index) + ", " + keyboard[row][col] + ": " + row + ", " + col + ": " + l);
+//            printSeenLetters(seen, index);
+            l++;
+            char goal = line.charAt(index), curLetter = keyboard[row][col];
+
+            for (int i = 0; i < dc.length; i++) {
+                int dR = row + dr[i], dC = col + dc[i];
+                char lastLetter = keyboard[dR-dr[i]][dC-dc[i]];
+                while (inBounds(dR, dC) && !seen[index][dR][dC]) {
+                    char thisLetter = keyboard[dR][dC];
+                    // the same letter as before
+                    if (thisLetter == lastLetter) {
+                        dR += dr[i];
+                        dC += dc[i];
+                        continue;
+                    }
+                    // if not the same letter as we started with
+                    if (curLetter != thisLetter) {
+                        seen[index][dR][dC] = true;
+                        if (thisLetter == goal) {
+//                            System.out.println("\n==> found: " + goal + " for index " + index + " after " + l + " steps " + dR + ", " + dC + "\n");
+                            if (index + 1 == length) {
+//                                System.out.println("returning: " + l);
+                                return l;
+                            }
+                            q.add(index+1);
+                            seen[index+1][dR][dC] = true;
+                        } else {
+                            q.add(index);
+                        }
+                        q.add(dR);
+                        q.add(dC);
+                        q.add(l);
+//                        System.out.println("  added: " + keyboard[dR][dC] + ": " + dR + ", " + dC + ": " + l);
+                        break;
+                    }
+                    dR += dr[i];
+                    dC += dc[i];
+                    lastLetter = thisLetter;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void printSeenLetters(boolean[][][] seen, int level) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < seen[level].length; i++) {
+            for (int j = 0; j < seen[level][i].length; j++) {
+                if (seen[level][i][j]) {
+                    s.append(" " + keyboard[i][j]);
+                }
+            }
+        }
+        System.out.println("\t- Seen:" + s.toString());
+    }
+
     private void printkeyboard() {
         for (int r = 0; r < keyboard.length; r++) {
             for (int c = 0; c < keyboard[r].length; c++) {
@@ -87,51 +166,7 @@ public class Keyboarding {
             System.out.println();
         }
     }
-    private void printadj() {
-        System.out.println(adj.length);
-        for (int r = 0; r < adj.length; r++) {
-            System.out.printf(" %2d ", r);
-        }
-        System.out.println();
-        for (int r = 0; r < adj.length; r++) {
-            for (int c = 0; c < adj[r].length; c++) {
-                if (adj[r][c] == Integer.MAX_VALUE) {
-                    System.out.printf("  - ");
-                } else {
-                    System.out.printf(" %2d ", adj[r][c]);
-                }
-            }
-            System.out.println();
-        }
-    }
-    private void printgraph() {
-        System.out.println("GRAPH");
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i] == Integer.MAX_VALUE) {
-                System.out.printf("  - ");
-            } else {
-                System.out.printf(" %2d ", graph[i]);
-            }
-        }
-        System.out.println();
-    }
-    public void print(int[][] arr) {
-        System.out.println("TEMP");
-        for (int r = 0; r < arr.length; r++) {
-            for (int c = 0; c < arr.length; c++) {
-                System.out.printf(" %10d ", arr[r][c]);
-            }
-            System.out.println();
-        }
-    }
-    public void print(int[] arr, String string) {
-        System.out.println(string);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf(" %10d ", arr[i]);
 
-        }
-        System.out.println();
-    }
     // initialize data structures
     private void init(String line) {
         String[] temp = line.split("\\s+");
@@ -155,7 +190,7 @@ public class Keyboarding {
         return ((r >= 0 && r < R) && (c >= 0 && c < C));
     }
     // perform bfs with sRow and sCol as the source coordinates
-    private void bfs(int sRow, int sCol) {
+    private void bbfs(int sRow, int sCol) {
         int source = indexOf(sRow, sCol);
         boolean[][] seen = new boolean[keyboard.length][keyboard[0].length];
         Queue<Integer> q = new LinkedList<Integer>();
@@ -194,10 +229,9 @@ public class Keyboarding {
     }
     // build the initial adjacency matrix
     private void build() {
-        //int index = 0;
         for (int r = 0; r < R; r++) {
             for (int c = 0; c < C; c++) {
-                bfs(r, c);
+                bbfs(r, c);
             }
         }
     }
@@ -207,6 +241,7 @@ public class Keyboarding {
         char c;
         for (int i = 0; i < line.length(); i++) {
             c = line.charAt(i);
+
             numVert += num.get(c);
         }
         return numVert;
@@ -215,17 +250,13 @@ public class Keyboarding {
     private void buildInverse(String line, int numVert) {
         inverse = new int[numVert];
         char c;
-//        System.out.println("Indexes of: " + line);
         for (int i = 0, cursor = 1; i < line.length(); i++) {
             c = line.charAt(i);
             String[] block = indices.get(c).split("\\s+");
-//            System.out.println(c + ": " + block.length);
             for (int b = 0; b < block.length; b++) {
                 inverse[cursor++] = Integer.parseInt(block[b]);
-//                System.out.printf("%3d",inverse[cursor-1], b);
             }
         }
-//        System.out.println();
     }
     // build the graph
     private void buildGraph(String line) {
@@ -234,7 +265,6 @@ public class Keyboarding {
         buildInverse(line, numVert);
         long stop = System.currentTimeMillis();
         System.err.println("Set-Up: " + (stop - start));
-        // dijkstra on graph
         graph = new int[numVert];
         Arrays.fill(graph, Integer.MAX_VALUE);
         graph[0] = 0; // source to itself is 0;
@@ -249,7 +279,6 @@ public class Keyboarding {
                 if (vSource == Integer.MAX_VALUE) { continue; }
                 for (int to = 0; to < sizeBlock; to++) {
                     int t = h + to, dist = adj[inverse[s]][inverse[t]];
-//                    System.out.println("adj["+inverse[s]+"]["+inverse[t]+"] = "+dist);
                     if (dist != Integer.MAX_VALUE && (dist + vSource < graph[t])) {
                         graph[t] = dist + vSource;
                     }
